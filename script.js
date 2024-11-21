bbff1df97c69409a0b4de8aea4dc4f96dbc9bbf6
aldymz <aldimazmudien@gmail.com>
2024-11-21 19:04:23 +0700

    pesan

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-
-function openModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
-
-function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
-
-// Tutup modal jika area luar diklik
-window.onclick = function(event) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
-};
-
 function openModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = "block";
